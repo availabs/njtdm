cb111638a306172c00b3344080e72acdcac34612
Alex <am3081@gmail.com>
2014-02-17 11:19:10 -0500

    moving data

--- tdmData/cliRunModel.php
+++ tdmData/cliRunModel.php
@@ -1,2 +1,8 @@
 <?php
- echo "Test 3"; 
+ $dbh = pg_connect("host=localhost dbname=tdmData user=postgres");
+
+ $sql = "SELECT trips FROM triptable where id = ".$argv[1];
+ $result = pg_query($dbh, $sql);
+ $row = pg_fetch_array($result);
+ $trips = json_decode($row['trips']);
+ print_r($trips);
