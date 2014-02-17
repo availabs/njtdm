e55fbc11283885b3f6a95110d9c8aa6017fa4cf2
Alex <am3081@gmail.com>
2014-02-17 12:09:49 -0500

    run the model

--- tdmData/cliRunModel.php
+++ tdmData/cliRunModel.php
@@ -5,4 +5,66 @@
  $result = pg_query($dbh, $sql);
  $row = pg_fetch_array($result);
  $trips = json_decode($row['trips']);
- print_r($trips);
+ $model = array();
+ $model['successful_trips'] = array();
+ $model['failed_trips'] = array();
+ foreach($trips as $trip){
+ 	planTrip($trip['from_coords'][0],$trip['from_coords'][1],$trip['to_coords'][0],$trip['to_coords'][1],$trip[$time],$trip);
+ }
+ echo json_encode($model);
+ echo "FINISHED"
+ 
+
+function  planTrip ($from_lat,$from_lon,$to_lat,$to_lon,$departure_time,$trip){
+	
+
+	$otp_url = "http://localhost:8080/opentripplanner-api-webapp/ws/plan?";
+	$otp_url .= "fromPlace=$from_lat,$from_lon";
+	$otp_url .= "&toPlace=$to_lat,$to_lon";
+	$otp_url .= "&mode=TRANSIT,WALK";
+  	$otp_url .= "&min=QUICK";
+  	$otp_url .= "&maxWalkDistance=800";
+  	$otp_url .= "&walkSpeed=1.341";
+  	$otp_url .= "&time=$departure_time";
+  	$otp_url .= "&date=7/23/2013";
+  	$otp_url .= "&arriveBy=false";
+  	$otp_url .= "&itinID=1";
+  	$otp_url .= "&wheelchair=false";
+  	$otp_url .= "&preferredRoutes=";
+  	$otp_url .= "&unpreferredRoutes=";
+  	
+  	//echo $otp_url.'<br>';
+  // 	//echo 'Running trip at: time:'.rand($this->start_hour,$this->end_hour).':'.rand(0,59).'am<br><br>';
+
+  processTrip(json_decode(curl_download($otp_url),$trip));
+}
+
+function processTrip($data,$trip){
+
+	global $model;	
+	if(count($data['plan']['itineraries']) > 0){
+		array_push($model['successful_trips'],$data['plan']['itineraries'][rand(0,count($data['plan']['itineraries'])-1)]);
+	}else{
+
+		array_push($model['failed_trips'],$trip);
+	}
+}
+
+function curl_download($Url){ 
+    // is cURL installed yet?
+    if (!function_exists('curl_init')){
+        die('Sorry cURL is not installed!');
+    }
+ 
+  	// OK cool - then let's create a new cURL resource handle
+  	
+   	$ch = curl_init();
+   	$headers = array('Accept: application/json');
+   	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
+	//curl_setopt($ch, CURLOPT_HEADER, 1); 
+   	curl_setopt($ch, CURLOPT_URL, $Url);
+   	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+   	$output = curl_exec($ch);
+
+    return $output;
+}
