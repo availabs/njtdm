68d962e339b06a5a3b3e52a5172acd9c3c6a1d88
Alex <am3081@gmail.com>
2014-02-17 12:13:59 -0500

    test

--- tdmData/cliRunModel.php
+++ tdmData/cliRunModel.php
@@ -9,6 +9,7 @@
  $model['successful_trips'] = array();
  $model['failed_trips'] = array();
  foreach($trips as $trip){
+ 	echo $trip['from_coords'][0]." ".$trip['from_coords'][1]." ".$trip['to_coords'][0]." ".$trip['to_coords'][1]." ".$trip['time'];
  	planTrip($trip['from_coords'][0],$trip['from_coords'][1],$trip['to_coords'][0],$trip['to_coords'][1],$trip['time'],$trip);
  }
  echo json_encode($model);
