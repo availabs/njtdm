732f3e6dd521cfd1fb681669c3957ee8a06724ab
Alex <am3081@gmail.com>
2014-02-26 13:47:43 -0500

    stuff

--- tdmData/api/controllers/TractsController.js
+++ tdmData/api/controllers/TractsController.js
@@ -156,7 +156,8 @@ ctppTrips : function(req,res){
 		}else if(odtype == "stops"){
 			getStopsOD(fips_in,function(stop_points){
 				var id = 0;
-				
+				tracts_data.rows.forEach(function(tract){
+					
 					num_trips = tract.bus_total;
 					for(var i = 0; i < num_trips;i++){
 						var trip = {};
