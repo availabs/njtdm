dbf0884f1160d60f3c848e74423da0db934865b5
Alex <am3081@gmail.com>
2014-02-26 10:47:23 -0500

    test

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -210,7 +210,7 @@ angular.module( 'njTDM.home', [
       for(var tract in censusData.acs){
         busdata[tract] = censusData.acs[tract].bus_to_work/censusData.acs[tract].travel_to_work_total;
       }
-      promise = $http.post($scope.api+'tracts/lehdTrips', {tracts:$scope.tracts,buspercent:busdata,od:$scope.model_od}).then(function(data){
+      promise = $http.post($scope.api+'tracts/ctpp', {tracts:$scope.tracts,buspercent:busdata,od:$scope.model_od}).then(function(data){
         return data;
       });
 
