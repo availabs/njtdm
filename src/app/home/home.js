84b12dddb7a0e5d41a9e38027f33624e81e4c814
Alex <am3081@gmail.com>
2014-02-27 12:56:34 -0500

    stuff

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -66,8 +66,8 @@ angular.module( 'njTDM.home', [
  * CONTROLLER
  */
 .controller( 'HomeCtrl', function HomeController( $scope,$http,leafletData,$filter,Scenario,TripTable,$modal) {
-  //$scope.api = 'http://lor.availabs.org:1338/';
-  $scope.api = 'http://localhost:1337/';
+  $scope.api = 'http://lor.availabs.org:1338/';
+  //$scope.api = 'http://localhost:1337/';
   $scope.current_template_index = 0;
   $scope.model_time = 'am';
   $scope.census_vars = censusData.variables;
