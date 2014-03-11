c147bde2ab39da8f75b238585994ed1840536136
Alex <am3081@gmail.com>
2014-03-11 10:08:29 -0400

    stuff

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -395,11 +395,11 @@ angular.module( 'njTDM.home', [
   //***************************************************************************************************
   //***************************************************************************************************
   //***************************************************************************************************
-})
+});
 
 
 //--------------------------------------------------------------
-.controller( 'ModalInstanceCtrl', function ($scope, $modalInstance) {
+var ModalInstanceCtrl = function ($scope, $modalInstance) {
   
   $scope.ok = function (info) {
     $modalInstance.close(info);
@@ -408,4 +408,4 @@ angular.module( 'njTDM.home', [
   $scope.cancel = function () {
     $modalInstance.dismiss('cancel');
   };
-});
+};
