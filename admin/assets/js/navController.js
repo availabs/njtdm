d04468da82cd750d885fd5c8e08f179dbec31abe
alex <amuro@albany.edu>
2014-08-13 09:16:06 -0400

    tracking jobs with sockets

--- admin/assets/js/navController.js
+++ admin/assets/js/navController.js
@@ -1,12 +1,15 @@
 function navController($scope) {
 
 	io.socket.on('connect',function(){
-	    io.socket.get('/job',{},function(data){ 
+	    io.socket.get('/job',{'where': {"finished":"false"}},function(data){ 
 	        console.log('Jobs :',data); 
 	    });
-	    io.socket.on('Job', function(message){console.log('nav message',message)});
+	    io.socket.on('job', function(message){console.log('nav message',message)});
 	    io.socket.on('hello', function(data) {
 	      console.log('hello',data);
+	    });
+	    io.socket.on('job_created', function(data) {
+	      console.log('jc',data);
 	    });
     })
    
