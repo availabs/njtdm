1eda9cb76985933bab63524d2c3328c3c7461d16
alex <amuro@albany.edu>
2014-09-24 11:12:34 -0400

    JSON defined'

--- admin/assets/js/pages/ma-models.js
+++ admin/assets/js/pages/ma-models.js
@@ -166,7 +166,7 @@ function modelPageCtrl($scope){
     $scope.model.marketareaId = $scope.marketarea.id;
     
     $scope.model_processing = 'Processing Model...Please Wait'
-    d3.json('/triptable/run').post(json.stringify({model:$scope.model}),function(err,data){
+    d3.json('/triptable/run').post(JSON.stringify({model:$scope.model}),function(err,data){
         if(err){console.log(err);$scope.model_processing = err;}
         else{
       
