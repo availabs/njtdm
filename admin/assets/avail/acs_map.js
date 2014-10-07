ba6fa4ae9638a4422ebc311fa9614057e6344b97
alex <amuro@albany.edu>
2014-10-07 16:48:37 -0400

    ie

--- admin/assets/avail/acs_map.js
+++ admin/assets/avail/acs_map.js
@@ -77,6 +77,7 @@
 			.projection(projection);
 
         d3.json('/data/tracts.json', function(error, data) {
+        	console.log('hi eric what does this say? -->',data,'or error',error);
             if(typeof data != 'undefined'){
 	            data.features.forEach(function(feat){
 	                if(input_tracts.indexOf(feat.properties.geoid) !== -1){
