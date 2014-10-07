9d459166514a7b1d3ebcb1a3d7475c4ac0d90508
alex <amuro@albany.edu>
2014-10-07 13:49:40 -0400

    stuff

--- admin/assets/avail/acs_map.js
+++ admin/assets/avail/acs_map.js
@@ -232,13 +232,13 @@
 			else if (dataDomain.byPercent) {
 				var tractTotal = ACSgroups[currentGroup].map(function(cat) { return ACSdata[geoid][cat]; }).reduce(function(p, c) { return p+c || p }, 0);
 
-				//console.log('tractTotal',tractTotal);
+				
 				if (tractTotal === 0) {
 					value = 0;
 				}
 				else {
 					value = (value/tractTotal)*100 || 0;
-					console.log(value);
+					
 				}
 			}
 
